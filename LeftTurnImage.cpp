d96004dfc36e0641a5cb552ac51fd4c8945e7fdd
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-11-06 16:53:11 +0300

    Add: function for turn images

---
+++ LeftTurnImage.cpp
@@ -0,0 +1,25 @@
+//
+
+#include <iostream>
+#include "Image.h"
+#include "TurnImage.h"
+#include <vector>
+
+void Turn_Image::LeftTurn(const char* path)
+{
+    Image image(0, 0);
+    image.Read(path);
+    int height = image.GetHeight();
+    int width = image.GetWidth();
+    Image turn_image(height, width);
+    for(int y = 0; y < height; ++y)
+    {
+        for(int x = 0; x < width; ++x)
+        {
+            turn_image.SetColor(image.GetColor(x, y), height-  y - 1, x);
+        }
+    }
+    std::cout << "Left turn image created" << std::endl;
+    turn_image.Export("Left_turn_image.bmp");
+}
+
---
+++ RightTurnImage.cpp
@@ -0,0 +1,25 @@
+//
+
+#include <iostream>
+#include "Image.h"
+#include "TurnImage.h"
+#include <vector>
+
+void Turn_Image::RightTurn(const char* path)
+{
+    Image image(0, 0);
+    image.Read(path);
+    int height = image.GetHeight();
+    int width = image.GetWidth();
+    Image turn_image(height, width);
+    for(int y = 0; y < height; ++y)
+    {
+        for(int x = 0; x < width; ++x)
+        {
+            turn_image.SetColor(image.GetColor(x, y), y, width - x - 1);
+        }
+    }
+    std::cout << "Right turn image created" << std::endl;
+    turn_image.Export("Right_turn_image.bmp");
+}
+
