04d3f752e152838dc44847dfc6b51a6954becbd3
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-11-06 16:54:22 +0300

    Add: header file for functios for turn images

---
+++ TurnImage.h
@@ -0,0 +1,14 @@
+//Uljana Alexejewa st129990@student.spbu.ru
+
+
+#ifndef TURNIMAGE_H
+#define TURNIMAGE_H
+
+namespace Turn_Image
+{
+    void RightTurn(const char* path);
+    void LeftTurn(const char* path);
+}
+
+
+#endif // TURNIMAGE_H
