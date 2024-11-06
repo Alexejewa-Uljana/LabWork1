a8c52b135d8d64f04a6cd55bf2dfe0e4e36767c6
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-11-06 20:01:32 +0300

    Add: function for generate Gaussian Kernel

---
+++ Kernel.h
@@ -0,0 +1,13 @@
+//Uljana Alexejewa st129990@student.spbu.ru
+
+
+#ifndef KERNEL_H
+#define KERNEL_H
+
+namespace Gauss_Kernel
+{
+    std::vector<std::vector<float>> GenerateGaussianKernel(int radius, float sigma);
+}
+
+
+#endif // KERNEL_H
