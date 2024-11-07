b5bfb1c364b6e3ab86f306674b767267611e092c
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-11-07 09:48:53 +0300

    Fix: add reading of file path and function values

--- main.cpp
+++ main.cpp
@@ -2,20 +2,28 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include "Image.h"
 #include "TurnImage.h"
 #include "Kernel.h"
 
 int main(int argc, char* argv [])
 {
-    Turn_Image::RightTurn("123.bmp");
-    Turn_Image::LeftTurn("123.bmp");
+    std::cout << "Enter path to file, radius and sigma for Gauss blur" << std::endl;
+    std::string path_string;
+    std::getline(std::cin, path_string);
+    const char* path = path_string.c_str();
+    int radius;
+    float sigma;
+    std::cin >> radius >> sigma;
+    Turn_Image::RightTurn(path);
+    Turn_Image::LeftTurn(path);
     Image image_left(0, 0);
     image_left.Read("Left_turn_image.bmp");
-    image_left.GaussianBlur(10, 10.0f);
+    image_left.GaussianBlur(radius, sigma);
     image_left.Export("Blurring_left_turn_image.bmp");
     Image image_right(0, 0);
     image_right.Read("Right_turn_image.bmp");
-    image_right.GaussianBlur(10, 10.0f);
+    image_right.GaussianBlur(radius, sigma);
     image_right.Export("Blurring_right_turn_image.bmp");
 }
