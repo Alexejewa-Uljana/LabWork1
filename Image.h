5106d0907a065464a6fa9da408301b88249f5d1e
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-11-22 20:12:31 +0300

    Fix: solve problem with information in header and reading and saving rectangular images

--- Image.h
+++ Image.h
@@ -9,14 +9,43 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
-struct Color {
+struct Color
+{
     float r, g, b;
     Color();
     Color(float r, float g, float b);
     ~Color();
 };
 
-class Image {
+#pragma pack(push, 1)
+struct BITMAPFILEHEADER
+{
+    uint16_t bfType;
+    uint32_t bfSize;
+    uint16_t bfReserved1;
+    uint16_t bfReserved2;
+    uint32_t bfOffBits;
+};
+
+struct BITMAPINFOHEADER
+{
+    uint32_t biSize;
+    int32_t biWidth;
+    int32_t biHeight;
+    uint16_t biPlanes;
+    uint16_t biBitCount;
+    uint32_t biCompression;
+    uint32_t biSizeImage;
+    int32_t biXPelsPerMeter;
+    int32_t biYPelsPerMeter;
+    uint32_t biClrUsed;
+    uint32_t biClrImportant;
+};
+#pragma pack(pop)
+
+
+class Image
+{
 public:
     Image(int width, int height);
     ~Image();
